ebab43187c5fdaf5e11b2a41adf567117b00d69f
walamoonbeam <mark.vanaken@gmail.com>
2014-03-19 14:01:29 -0400

    Added JUnit test for Card class
    There aren't any test cases yet, but the class is made

---
+++ src/test/CardTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author vanakema.
+ *         Created Mar 19, 2014.
+ */
+public class CardTest {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
