57ff383e5b1b648a64f794bc85f5201a19e48d46
walamoonbeam <mark.vanaken@gmail.com>
2014-04-03 15:50:24 -0400

    Moved card tests out of DeckTest
    Moved the CardFactory tests out of the DeckTest and into the CardTest
    class, where they belong.

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -9,6 +9,71 @@ import main.Card;
 import org.junit.Test;
 
 public class CardTest {
+  private void testFactoryForCardName(String name) {
+    Card card = Card.makeCard(name);
+    assertNotNull(card);
+    assertEquals(name, card.getName());
+}
+
+@Test
+public void testFactoryForFestivalCard() {
+    testFactoryForCardName(Card.CARD_NAME_FESTIVAL);
+}
+
+@Test
+public void testFactoryForLaboratory() {
+    testFactoryForCardName(Card.CARD_NAME_LABORATORY);
+}
+
+@Test
+public void testFactoryForMarket() {
+    testFactoryForCardName(Card.CARD_NAME_MARKET);
+}
+
+@Test
+public void testFactoryForSmithy() {
+    testFactoryForCardName(Card.CARD_NAME_SMITHY);
+}
+
+@Test
+public void testFactoryForVillage() {
+    testFactoryForCardName(Card.CARD_NAME_VILLAGE);
+}
+
+@Test
+public void testFactoryForWoodcutter() {
+    testFactoryForCardName(Card.CARD_NAME_WOODCUTTER);
+}
+
+@Test
+public void testFactoryForEstate() {
+    testFactoryForCardName(Card.CARD_NAME_ESTATE);
+}
+
+@Test
+public void testFactoryForDuchy() {
+    testFactoryForCardName(Card.CARD_NAME_DUCHY);
+}
+
+@Test
+public void testFactoryForProvince() {
+    testFactoryForCardName(Card.CARD_NAME_PROVINCE);
+}
+
+@Test
+public void testFactoryForCopper() {
+    testFactoryForCardName(Card.CARD_NAME_COPPER);
+}
+
+@Test
+public void testFactoryForSilver() {
+    testFactoryForCardName(Card.CARD_NAME_SILVER);
+}
+
+@Test
+public void testFactoryForGold() {
+    testFactoryForCardName(Card.CARD_NAME_GOLD);
+}
 
   @Test(expected = NoSuchElementException.class)
   public void makeInvalidCard(){
--- src/test/DeckTest.java
+++ src/test/DeckTest.java
@@ -11,73 +11,7 @@ import main.PlayerDeck;
 import org.junit.Test;
 
 public class DeckTest {
-	
-	private void testFactoryForCardName(String name) {
-		Card card = Card.makeCard(name);
-		assertNotNull(card);
-		assertEquals(name, card.getName());
-	}
-	
-	@Test
-	public void testFactoryForFestivalCard() {
-		testFactoryForCardName(Card.CARD_NAME_FESTIVAL);
-	}
-	
-	@Test
-	public void testFactoryForLaboratory() {
-		testFactoryForCardName(Card.CARD_NAME_LABORATORY);
-	}
-	
-	@Test
-	public void testFactoryForMarket() {
-		testFactoryForCardName(Card.CARD_NAME_MARKET);
-	}
-	
-	@Test
-	public void testFactoryForSmithy() {
-		testFactoryForCardName(Card.CARD_NAME_SMITHY);
-	}
-	
-	@Test
-	public void testFactoryForVillage() {
-		testFactoryForCardName(Card.CARD_NAME_VILLAGE);
-	}
-	
-	@Test
-	public void testFactoryForWoodcutter() {
-		testFactoryForCardName(Card.CARD_NAME_WOODCUTTER);
-	}
-
-	@Test
-	public void testFactoryForEstate() {
-		testFactoryForCardName(Card.CARD_NAME_ESTATE);
-	}
-	
-	@Test
-	public void testFactoryForDuchy() {
-		testFactoryForCardName(Card.CARD_NAME_DUCHY);
-	}
-	
-	@Test
-	public void testFactoryForProvince() {
-		testFactoryForCardName(Card.CARD_NAME_PROVINCE);
-	}
-	
-	@Test
-	public void testFactoryForCopper() {
-		testFactoryForCardName(Card.CARD_NAME_COPPER);
-	}
-	
-	@Test
-	public void testFactoryForSilver() {
-		testFactoryForCardName(Card.CARD_NAME_SILVER);
-	}
-	
-	@Test
-	public void testFactoryForGold() {
-		testFactoryForCardName(Card.CARD_NAME_GOLD);
-	}
-	
+		
 	@Test
 	public void sizeTest() {
 		PlayerDeck deck1 = makeDeck();
