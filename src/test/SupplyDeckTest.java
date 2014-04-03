2dc7c359680ffb9222a9bb7ace33aa7c2d84d606
walamoonbeam <mark.vanaken@gmail.com>
2014-04-03 16:12:41 -0400

    Added UserDescription test to SupplyDeckTest

--- src/test/SupplyDeckTest.java
+++ src/test/SupplyDeckTest.java
@@ -12,43 +12,54 @@ import main.SupplyDeck.CardTuple;
 import org.junit.Test;
 
 public class SupplyDeckTest {
-	
-	List<Card> cards = Arrays.asList(new Card[]{ Card.makeCard(Card.CARD_NAME_FESTIVAL),
-		     Card.makeCard(Card.CARD_NAME_LABORATORY),
-		     Card.makeCard(Card.CARD_NAME_MARKET),
-		     Card.makeCard(Card.CARD_NAME_SMITHY),
-		     Card.makeCard(Card.CARD_NAME_VILLAGE),
-		     Card.makeCard(Card.CARD_NAME_WOODCUTTER) });
 
-	@Test
-	public void testBuyIndex0Card() {
-		SupplyDeck supplyDeck = new SupplyDeck(cards);
-		Card festCard = supplyDeck.buyActionCardAtIndex(0);
-		assertEquals(festCard.getName(), Card.CARD_NAME_FESTIVAL);
-		assertEquals(supplyDeck.getActionCardRoster().get(0).getSupply(), 9);
-	}
-	
-	@Test
-	public void testBuyIndex3Card() {
-		SupplyDeck supplyDeck = new SupplyDeck(cards);
-		Card festCard = supplyDeck.buyActionCardAtIndex(3);
-		assertEquals(festCard.getName(), Card.CARD_NAME_SMITHY);
-		assertEquals(supplyDeck.getActionCardRoster().get(3).getSupply(), 9);
-	}
-	
-	@Test
-	public void testBuyIndex5Card() {
-		SupplyDeck supplyDeck = new SupplyDeck(cards);
-		Card festCard = supplyDeck.buyActionCardAtIndex(5);
-		assertEquals(festCard.getName(), Card.CARD_NAME_WOODCUTTER);
-		assertEquals(supplyDeck.getActionCardRoster().get(5).getSupply(), 9);
-	}
-	
-	@Test
-	public void testGetRoster() {
-		SupplyDeck supplyDeck = new SupplyDeck(cards);
-		List<CardTuple> cardTuples = supplyDeck.getActionCardRoster();
-		for (int i = 0; i < cards.size(); i++)
-			assertEquals(cardTuples.get(i).getCard(), cards.get(i));
-	}
+  List<Card> cards = Arrays.asList(new Card[] {Card.makeCard(Card.CARD_NAME_FESTIVAL),
+      Card.makeCard(Card.CARD_NAME_LABORATORY), Card.makeCard(Card.CARD_NAME_MARKET),
+      Card.makeCard(Card.CARD_NAME_SMITHY), Card.makeCard(Card.CARD_NAME_VILLAGE),
+      Card.makeCard(Card.CARD_NAME_WOODCUTTER)});
+
+  @Test
+  public void testBuyIndex0Card() {
+    SupplyDeck supplyDeck = new SupplyDeck(cards);
+    Card festCard = supplyDeck.buyActionCardAtIndex(0);
+    assertEquals(festCard.getName(), Card.CARD_NAME_FESTIVAL);
+    assertEquals(supplyDeck.getActionCardRoster().get(0).getSupply(), 9);
+  }
+
+  @Test
+  public void testBuyIndex3Card() {
+    SupplyDeck supplyDeck = new SupplyDeck(cards);
+    Card festCard = supplyDeck.buyActionCardAtIndex(3);
+    assertEquals(festCard.getName(), Card.CARD_NAME_SMITHY);
+    assertEquals(supplyDeck.getActionCardRoster().get(3).getSupply(), 9);
+  }
+
+  @Test
+  public void testBuyIndex5Card() {
+    SupplyDeck supplyDeck = new SupplyDeck(cards);
+    Card festCard = supplyDeck.buyActionCardAtIndex(5);
+    assertEquals(festCard.getName(), Card.CARD_NAME_WOODCUTTER);
+    assertEquals(supplyDeck.getActionCardRoster().get(5).getSupply(), 9);
+  }
+
+  @Test
+  public void testGetRoster() {
+    SupplyDeck supplyDeck = new SupplyDeck(cards);
+    List<CardTuple> cardTuples = supplyDeck.getActionCardRoster();
+    for (int i = 0; i < cards.size(); i++)
+      assertEquals(cardTuples.get(i).getCard(), cards.get(i));
+  }
+
+  @Test
+  public void testUserDescription() {
+    SupplyDeck supplyDeck = new SupplyDeck(cards);
+    List<CardTuple> cardTuples = supplyDeck.getActionCardRoster();
+    assertEquals(Card.CARD_NAME_FESTIVAL + " (10)", cardTuples.get(0).userDescription());
+    assertEquals(Card.CARD_NAME_LABORATORY + " (10)", cardTuples.get(1).userDescription());
+    assertEquals(Card.CARD_NAME_MARKET + " (10)", cardTuples.get(2).userDescription());
+    assertEquals(Card.CARD_NAME_SMITHY + " (10)", cardTuples.get(3).userDescription());
+    assertEquals(Card.CARD_NAME_VILLAGE + " (10)", cardTuples.get(4).userDescription());
+    assertEquals(Card.CARD_NAME_WOODCUTTER + " (10)", cardTuples.get(5).userDescription());
+
+  }
 }
