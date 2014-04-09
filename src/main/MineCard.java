032c3a28fab5995e7643360253dd64dcb08fe652
walamoonbeam <mark.vanaken@gmail.com>
2014-04-09 16:28:51 -0400

    Modified MineCard, ThroneRoomCard
    Added the code that allows for a choice to be shown.

--- src/main/MineCard.java
+++ src/main/MineCard.java
@@ -3,47 +3,29 @@ package main;
 public class MineCard extends ActionCard {
 
   protected MineCard() {
-    super(Card.CARD_NAME_MINE, "Trash a Treasure card from your hand. Gain a Treasure card costing up to 3 Coins more; put it into your hand.", 5);
-  }
-
-  @Override
-  public void addAdditionalActions(GameContext context) {
-
-  }
-
-  @Override
-  public void addAdditionalBuys(GameContext context) {
-
-  }
-
-  @Override
-  public void addAdditionalCoins(GameContext context) {
-
+    super(
+        Card.CARD_NAME_MINE,
+        "Trash a Treasure card from your hand. Gain a Treasure card costing up to 3 Coins more; put it into your hand.",
+        5);
   }
 
   @Override
   public void performAction(GameContext context) {
-    //TODO: Make so it takes an index, and checks if that is a proper treasure card
-    Card oldCard;
-    int handSize = context.getPlayer().getPlayerDeck().getHand().size();
-    for(int i = 0 ; i<handSize ; i++) {
-      oldCard = context.getPlayer().getPlayerDeck().getHand().get(i);
-      if(oldCard.getName().equals(CARD_NAME_COPPER)) {
-        boolean silverAdded = context.getTurnController().tryForceInsertResourceCardIntoHand(1);
-        if(silverAdded) {
-          context.getPlayer().getPlayerDeck().trashCardInHandAtIndex(i);
-          break;
+    //TODO: Testing on this
+    super.performAction(context);
+    int index = context.decideCardInHand("Choose a Treasure Card to trash", true);
+    if (index != GameContext.DecisionDelegate.CARD_IN_HAND_IGNORED) {
+      Card cardToTrash = context.getPlayer().getPlayerDeck().hand.get(index);
+      if(cardToTrash.getName() == Card.CARD_NAME_COPPER) {
+        if(context.getTurnController().tryForceInsertResourceCardIntoHand(1)) {
+          context.getPlayer().getPlayerDeck().trashCardInHandAtIndex(index);
         }
       }
-      else if(oldCard.getName().equals(CARD_NAME_SILVER)) {
-        boolean goldAdded = context.getTurnController().tryForceInsertResourceCardIntoHand(2);
-        if(goldAdded) {
-          context.getPlayer().getPlayerDeck().trashCardInHandAtIndex(i);
-          break;
+      else if(cardToTrash.getName() == Card.CARD_NAME_SILVER) {
+        if(context.getTurnController().tryForceInsertResourceCardIntoHand(2)) {
+          context.getPlayer().getPlayerDeck().trashCardInHandAtIndex(index);
         }
       }
     }
-
   }
-
 }
--- src/main/ThroneRoomCard.java
+++ src/main/ThroneRoomCard.java
@@ -6,11 +6,14 @@ public class ThroneRoomCard extends ActionCard {
     super(Card.CARD_NAME_THRONE_ROOM, "Choose an Action card in your hand. Play it twice.", 4);
   }
   
-  //TODO: Actually do it.
   @Override
   public void performAction(GameContext context) {
     super.performAction(context);
     int index = context.decideCardInHand("What card do you want to play twice?", true);
+    if (index != GameContext.DecisionDelegate.CARD_IN_HAND_IGNORED) {
+      context.getTurnController().forcePlayingCardAtIndex(index);
+      context.getTurnController().forcePlayingCardAtIndex(index);
+    }
     
   }
 
