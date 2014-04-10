3c90b2053bc42b740e908324eabd20db158efa90
Zach Hull <hullzr@rose-hulman.edu>
2014-04-10 17:14:51 -0400

    ChapelCardTest class

---
+++ src/test/ChapelCardTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import main.Card;
+import main.GameContext;
+import main.ChapelCard;
+import main.PlayerDeck;
+
+import org.junit.Test;
+
+public class ChapelCardTest {
+  
+    //ChapelCard card = new ChapelCard();
+    GameContext con = new GameContext();
+    
+  @Test
+  public void testName(){
+   // assertEquals("Chapel", card.getName());
+  }
+  
+  @Test
+  public void testCost(){
+    //assertEquals(2,card.getCost());
+  }
+  
+  @Test
+  public void testDescription(){
+   // assertEquals("Trash up to 4 cards from your hand.", card.getDescription());
+  }
+  
+  @Test
+  public void testPerformAction(){
+  
+    
+  }
+  
+ 
+  
+  
+  
+  
+}
