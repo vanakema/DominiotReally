e864e57a3ab4b1fb04c91f4787bb62308c166409
James Savage <axiixc@gmail.com>
2014-04-10 16:10:45 -0400

    Forgot to add workshop card

---
+++ src/main/WorkshopCard.java
@@ -0,0 +1,16 @@
+package main;
+
+public class WorkshopCard extends ActionCard {
+
+  protected WorkshopCard() {
+    super(Card.CARD_NAME_WORKSHOP, "Gain a card costing up to 4.", 0);
+  }
+  
+  @Override
+  public void addAdditionalCoins(GameContext context) {
+    super.addAdditionalCoins(context);
+    
+    context.setLumpSumTreasureCount(4);
+  }
+
+}
