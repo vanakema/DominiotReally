3edc0fd8ecaffc4287c31641fe23e39189fa458f
Zach Hull <hullzr@rose-hulman.edu>
2014-04-10 17:51:19 -0400

    ChapelCardTest in progress

--- src/test/ChapelCardTest.java
+++ src/test/ChapelCardTest.java
@@ -2,44 +2,67 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.List;
+
+import junit.framework.TestCase;
 
 import main.Card;
 import main.GameContext;
 import main.ChapelCard;
+import main.Player;
 import main.PlayerDeck;
+import main.SupplyDeck;
+import main.TurnController;
 
 import org.junit.Test;
 
-public class ChapelCardTest {
-  
-    ChapelCard card = new ChapelCard();
-    GameContext con = new GameContext();
+public class ChapelCardTest extends TestCase {
+
+  Player player;
+  List<Card> hand;
+  TurnController controller;
+  SupplyDeck supplyDeck;
+  GameContext context;
+  ChapelCard card = new ChapelCard();
+
+  @Override
+  protected void setUp() {
+    player = new Player("Test Player");
     
+    List<Card> cards = Arrays.asList(new Card[] {Card.makeCard(Card.CARD_NAME_FESTIVAL),
+    Card.makeCard(Card.CARD_NAME_LABORATORY), Card.makeCard(Card.CARD_NAME_MARKET),
+    Card.makeCard(Card.CARD_NAME_SMITHY), Card.makeCard(Card.CARD_NAME_VILLAGE),
+    Card.makeCard(Card.CARD_NAME_WOODCUTTER)});
+    
+    supplyDeck = new SupplyDeck(cards);
+    controller = new TurnController(player, supplyDeck, null);
+    context = new GameContext(controller);
+  }
+
+
+
   @Test
-  public void testName(){
+  public void testName() {
     assertEquals("Chapel", card.getName());
   }
-  
+
   @Test
-  public void testCost(){
-    assertEquals(2,card.getCost());
+  public void testCost() {
+    assertEquals(2, card.getCost());
   }
-  
+
   @Test
-  public void testDescription(){
+  public void testDescription() {
     assertEquals("Trash up to 4 cards from your hand.", card.getDescription());
   }
-  
+
   @Test
-  public void testPerformAction(){
-  
+  public void testPerformAction() {
     
+
   }
-  
- 
-  
-  
-  
-  
+
+
+
 }
