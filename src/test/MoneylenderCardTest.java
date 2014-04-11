b0ea7709301976203203e9e6b9b1e06b26bec0c0
Zach Hull <hullzr@rose-hulman.edu>
2014-04-10 20:38:46 -0400

    MoneylenderCardTest completed

--- src/test/MoneylenderCardTest.java
+++ src/test/MoneylenderCardTest.java
@@ -6,12 +6,12 @@ import java.util.List;
 import org.junit.Test;
 
 import main.Card;
-import main.ChapelCard;
 import main.GameContext;
 import main.MoneylenderCard;
 import main.Player;
 import main.SupplyDeck;
 import main.TurnController;
+import main.GameContext.DecisionDelegate;
 import junit.framework.TestCase;
 
 public class MoneylenderCardTest extends TestCase{
@@ -50,6 +50,38 @@ public class MoneylenderCardTest extends TestCase{
   @Test
   public void testDescription() {
     assertEquals("Trash a Copper from your hand. If you do, +$3.", card.getDescription());
+  }
+  
+  @Test
+  public void testPerformAction(){
+    context.setDecisionDelegate(new DecisionDelegate() {
+      int count =0;
+      @Override
+      public int decideCardInHand(GameContext context, String question, boolean canIgnore) {
+        if(count == 4) {
+          
+          return GameContext.DecisionDelegate.CARD_IN_HAND_IGNORED;
+        }
+        else {
+          count++;
+          return 0;
+        }
+      }
+      
+      @Override
+      public boolean decideBoolean(GameContext context, String question) {
+        return false;
+      }
+    });
+    context.getPlayer().getPlayerDeck().drawNumAndDiscardOldHand(10);
+    List<Card> hand = context.getPlayer().getPlayerDeck().getHand();
+    int handSize = hand.size();
+    assertEquals(handSize,10);
+    
+    Card moneylender = Card.makeCard(Card.CARD_NAME_MONEYLENDER);
+    moneylender.performAction(context);
+    
+    assertEquals(context.getTreasureCount(),3);
   }
   
 
