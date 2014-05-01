3b003098e54c7f7c18e5a7235b192b5fe825e166
walamoonbeam <mark.vanaken@gmail.com>
2014-05-01 17:36:57 -0400

    Made empty ThiefCard

---
+++ src/main/cards/ThiefCard.java
@@ -0,0 +1,10 @@
+package main.cards;
+
+public class ThiefCard extends ActionCard {
+
+  public ThiefCard(String name, String description, int cost) {
+    super(name, description, cost);
+    // TODO Auto-generated constructor stub
+  }
+
+}
