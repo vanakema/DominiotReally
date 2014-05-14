d301ad379f954021c67b528de61663e8582fc495
Zach Hull <hullzr@rose-hulman.edu>
2014-05-14 19:45:37 -0400

    LibraryCard in progress

--- src/main/cards/LibraryCard.java
+++ src/main/cards/LibraryCard.java
@@ -1,11 +1,20 @@
 package main.cards;
 
+import main.GameContext;
+
 public class LibraryCard extends ActionCard {
 
   protected LibraryCard() {
     super(Card.CARD_NAME_LIBRARY, "ad", 5);
   }
-  
-  
+ 
 
+  @Override
+  public void performAction(GameContext context) {
+    super.performAction(context);
+    
+    
+    
+  }
+  
 }
