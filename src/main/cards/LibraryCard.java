29029819a6e8844b163c4623e551129a8caacb85
Zach Hull <hullzr@rose-hulman.edu>
2014-05-15 19:06:00 -0400

    Library card needs tested

--- src/main/cards/LibraryCard.java
+++ src/main/cards/LibraryCard.java
@@ -1,20 +1,35 @@
 package main.cards;
 
+import java.util.List;
+
 import main.GameContext;
+import main.PlayerDeck;
 
 public class LibraryCard extends ActionCard {
 
   protected LibraryCard() {
-    super(Card.CARD_NAME_LIBRARY, "ad", 5);
+    super(Card.CARD_NAME_LIBRARY, "", 5);
   }
- 
+
 
   @Override
   public void performAction(GameContext context) {
     super.performAction(context);
     
+    PlayerDeck deck = context.getPlayer().getPlayerDeck();
+    List<Card> currentHand = context.getPlayer().getPlayerDeck().getHand();
     
-    
+   
+    while(currentHand.size()<7){
+      Card currentCard = deck.getDrawDeck().get(0);
+      if(currentCard instanceof ActionCard){
+        if(context.decideBoolean("Would you like to add "+ currentCard.getName() + " to your hand?")){
+         deck.drawNum(1); 
+        }else{
+          deck.getDiscardDeck().add(deck.getDrawDeck().remove(0));
+        }
+        deck.drawNum(1);
+      }
+    }    
   }
-  
 }
